2fe6554f0ee1224c9d9578279e5d87f79be2d32c
kizuner-bonely <1003306162@qq.com>
2025-12-01 07:11:17 +0800

    feat: implement upload and read files

--- lib/r2.ts
+++ lib/r2.ts
@@ -1,6 +1,11 @@
 'use server'
-
-import { S3Client } from '@aws-sdk/client-s3'
+import type { Readable } from 'stream'
+import { catchError, firstValueFrom, from, map, mergeMap, of } from 'rxjs'
+import {
+  GetObjectCommand,
+  PutObjectCommand,
+  S3Client,
+} from '@aws-sdk/client-s3'
 
 export const r2 = new S3Client({
   region: 'auto',
@@ -10,3 +15,47 @@ export const r2 = new S3Client({
     secretAccessKey: process.env.R2_SECRET_KEY!,
   },
 })
+
+export async function uploadFile(file: File, type: 'courses' | 'users') {
+  return await firstValueFrom(
+    from(file.arrayBuffer()).pipe(
+      mergeMap((buffer) => {
+        const key = `${type}/${Date.now()}-${file.name}`
+        const command = new PutObjectCommand({
+          Key: key,
+          ContentType: file.type,
+          Body: Buffer.from(buffer),
+          Bucket: process.env.R2_BUCKET!,
+        })
+
+        return from(r2.send(command))
+      }),
+      map(() => {
+        return { success: true, msg: null }
+      }),
+      catchError((error) => {
+        return of({ success: false, msg: error.message })
+      }),
+    ),
+  )
+}
+
+export async function readFile(key: string) {
+  return await firstValueFrom(
+    from(
+      r2.send(
+        new GetObjectCommand({
+          Bucket: process.env.R2_BUCKET!,
+          Key: key,
+        }),
+      ),
+    ).pipe(
+      map((res) => {
+        return { success: true, data: res.Body as Readable }
+      }),
+      catchError((error) => {
+        return of({ success: false, msg: error.message })
+      }),
+    ),
+  )
+}
